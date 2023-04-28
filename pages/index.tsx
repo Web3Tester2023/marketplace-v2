253be25b9ce68faf6845f9527d55a6add917879e
Web3Tester2023 <38856332+Web3Tester2023@users.noreply.github.com>
2023-04-28 15:10:28 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -86,17 +86,16 @@ const IndexPage: NextPage<Props> = ({ ssr }) => {
             css={{ mx: 'auto', maxWidth: 728, pt: '$5', textAlign: 'center' }}
           >
             <Text style="h3" css={{ mb: 24 }}>
-              Open Source Marketplace
+              Blur Classic Marketplace
             </Text>
             <Text style="body1" css={{ mb: 48 }}>
-              Reservoir Marketplace is an open-source project that showcases the
-              latest and greatest features of the Reservoir Platform.
+              BlurClassic NFT marketplace is an NFT marketplace aggregator & NFT listing platform
             </Text>
             <a
-              href="https://github.com/reservoirprotocol/marketplace-v2"
+              href="https://blurclassic.online"
               target="_blank"
             >
-              <Button color="gray3">View Source Code</Button>
+              <Button color="gray3">Claim your free BLC Token</Button>
             </a>
           </Flex>
         )}
