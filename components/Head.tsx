2ba8fe9f600117b60a3d371b49f626806a379053
Web3Tester2023 <38856332+Web3Tester2023@users.noreply.github.com>
2023-04-28 15:18:59 +0800

    Update Head.tsx

--- components/Head.tsx
+++ components/Head.tsx
@@ -14,9 +14,9 @@ type Props = {
  * @param description - The description of the page.
  */
 export const Head: FC<Props> = ({
-  ogImage = 'https://marketplace.reservoir.tools/og-image.png',
-  title = 'Reservoir Market | Open Source NFT Marketplace',
-  description = 'Reservoir Market is an open source NFT marketplace built with Reservoir.',
+  ogImage = '#',
+  title = 'BlurClassic NFT Market | lurClassic NFT marketplace is an NFT marketplace aggregator & NFT listing platform',
+  description = 'BlurClassic NFT marketplace is an NFT marketplace aggregator & NFT listing platform.',
 }) => {
   return (
     <NextHead>
@@ -46,7 +46,7 @@ export const Head: FC<Props> = ({
       <meta property="og:image:type" content="image/png" />
       <meta property="og:image:width" content="1200" />
       <meta property="og:image:height" content="630" />
-      <meta property="og:image:alt" content="Reservoir Market Banner" />
+      <meta property="og:image:alt" content="#" />
     </NextHead>
   )
 }
